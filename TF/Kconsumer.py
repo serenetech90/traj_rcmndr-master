705d106ac5ca872e49b6a6dc94da678a7dba6ca6
serenetech90 <serenetech90@gmail.com>
2021-11-27 17:02:03 +0200

    tf2.0 implementation with json processing of data files

--- TF/Kconsumer.py
+++ TF/Kconsumer.py
@@ -1,14 +1,10 @@
 import confluent_kafka as ck
-from confluent_kafka import Producer
-import socket
-from ksql import KSQLAPI as kapi
 
-conf = {'bootstrap.servers': "host1:9092, host2:9092", 'client.id': socket.gethostname()}
-p = Producer(config=conf)
+from ksql import KSQLAPI as kapi
 
 kclient = kapi('https://localhost:8088')
 kclient.create_table(table_name='coordinates_data', columns_type=['ped_x float32', 'ped_y float32'], value_format='JSON',
                      topic='trajectories',
                      key='ped_id')
 
-kclient.query()
+kclient.query('select * from coordinates_data')
